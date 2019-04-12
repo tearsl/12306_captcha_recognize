61339bdd07308d6b62df2a5ed4419a39296dcc14
novio <744351893@qq.com>
2019-04-12 14:58:44 +0800

    使用另一套方案进行Hash化，mAP:0.85

---
+++ DeepHash/train.py
@@ -0,0 +1,127 @@
+import os
+import shutil
+import argparse
+
+import torch
+import torch.nn as nn
+import sys
+sys.path.insert(0,os.curdir)
+from net import AlexNetPlusLatent
+
+from torchvision import datasets, models, transforms
+from torch.autograd import Variable
+import torch.optim.lr_scheduler
+
+
+parser = argparse.ArgumentParser(description='Deep Hashing')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                    help='SGD momentum (default: 0.9)')
+parser.add_argument('--epoch', type=int, default=128, metavar='epoch',
+                    help='epoch')
+parser.add_argument('--pretrained', type=int, default=0, metavar='pretrained_model',
+                    help='loading pretrained model(default = None)')
+parser.add_argument('--bits', type=int, default=48, metavar='bts',
+                    help='binary bits')
+parser.add_argument('--path', type=str, default='model', metavar='P',
+                    help='path directory')
+args = parser.parse_args()
+
+best_acc = 0
+start_epoch = 1
+transform_train = transforms.Compose(
+    [transforms.Resize(256),
+     transforms.RandomCrop(227),
+     transforms.RandomHorizontalFlip(),
+     transforms.ToTensor(),
+     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+transform_test = transforms.Compose(
+    [transforms.Resize(227),
+     transforms.ToTensor(),
+     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+trainset = datasets.CIFAR10(root='../cifar-10-python', train=True, download=False,
+                            transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=72,
+                                          shuffle=True, num_workers=0)
+
+testset = datasets.CIFAR10(root='../cifar-10-python', train=False, download=False,
+                           transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=72,
+                                         shuffle=True, num_workers=0)
+
+net = AlexNetPlusLatent(args.bits)
+
+use_cuda = torch.cuda.is_available()
+
+if use_cuda:
+    net.cuda()
+
+softmaxloss = nn.CrossEntropyLoss().cuda()
+
+optimizer4nn = torch.optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=0.0005)
+
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer4nn, milestones=[64], gamma=0.1)
+
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        inputs, targets = Variable(inputs), Variable(targets)
+        _, outputs = net(inputs)
+        loss = softmaxloss(outputs, targets)
+        optimizer4nn.zero_grad()
+
+        loss.backward()
+
+        optimizer4nn.step()
+
+        train_loss += softmaxloss(outputs, targets).item()
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        print(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (train_loss/(batch_idx+1), 100*int(correct)/int(total), correct, total))
+    return train_loss/(batch_idx+1)
+
+def test():
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(testloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        inputs, targets = Variable(inputs), Variable(targets)
+        _, outputs = net(inputs)
+        loss = softmaxloss(outputs, targets)
+        test_loss += loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        print(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (test_loss/(batch_idx+1), 100*int(correct)/int(total), correct, total))
+    acc = 100*int(correct) / int(total)
+    if epoch == args.epoch:
+        print('Saving')
+        if not os.path.isdir('{}'.format(args.path)):
+            os.mkdir('{}'.format(args.path))
+        torch.save(net.state_dict(), './{}/{}'.format(args.path, acc))
+
+if args.pretrained:
+    net.load_state_dict(torch.load('./{}/{}'.format(args.path, args.pretrained)))
+    test()
+else:
+    if os.path.isdir('{}'.format(args.path)):
+        shutil.rmtree('{}'.format(args.path))
+    for epoch in range(start_epoch, start_epoch+args.epoch):
+        train(epoch)
+        test()
+        scheduler.step()
