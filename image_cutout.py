2ce79ee5be7afd63676c9c2b03560de5fbc5b58f
novio <744351893@qq.com>
2019-04-12 15:33:00 +0800

    抠图的时候调整为非灰度图

--- image_cutout.py
+++ image_cutout.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-import numpy as np
 from PIL import Image
 from tqdm import tqdm
 
@@ -22,15 +21,19 @@ def cutout():
             pic_desc_name = m_pic_prefix_name + '_desc' + m_pic_suffix_name
             if os.path.isfile(cur_path):
                 im = Image.open(cur_path)
-                # 将图片灰度化
-                gray = im.convert('L')
-                np_gray = np.array(gray)
+
+                # 取消灰度化
+                # # 将图片灰度化
+                # gray = im.convert('L')
+                # np_gray = np.array(gray)
+
                 # 获得降色阶后的灰度图
                 # 暂时不需要进行降色阶
                 # Image.fromarray(np.array(np_gray / 16, dtype='uint8'))
+
                 # 获得文字图片
                 to_save_path_word = os.path.join(pics_target_dir, pic_desc_name)
-                Image.fromarray(np_gray[0:30, 120:120 + 80]).save(to_save_path_word)
+                Image.fromarray(im[0:30, 120:120 + 80, :]).save(to_save_path_word)
 
                 # 分别抠图获得小图片
                 for j in range(4):
@@ -38,8 +41,8 @@ def cutout():
                                                      m_pic_prefix_name + '_' + str(j + 1) + m_pic_suffix_name)
                     to_save_path_row2 = os.path.join(pics_target_dir,
                                                      m_pic_prefix_name + '_' + str(j + 5) + m_pic_suffix_name)
-                    Image.fromarray(np_gray[42:42 + 66, 5 + dist[j]:5 + dist[j] + 66]).save(to_save_path_row1)
-                    Image.fromarray(np_gray[115:115 + 66, 5 + dist[j]:5 + dist[j] + 66]).save(to_save_path_row2)
+                    Image.fromarray(im[42:42 + 66, 5 + dist[j]:5 + dist[j] + 66, :]).save(to_save_path_row1)
+                    Image.fromarray(im[115:115 + 66, 5 + dist[j]:5 + dist[j] + 66, :]).save(to_save_path_row2)
                     to_write_mapping.write('%s,%s\n%s,%s\n' % (
                         to_save_path_word,
                         to_save_path_row1,
